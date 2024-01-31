a88582aefbc354e5bc186e6af920d5d4cb619885
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-01-31 18:59:12 +0200

    Fruit

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 11);
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.y< -7.3)
+        {
+            print(":(");
+            Destroy(gameObject);
+        }
+    }
+}
