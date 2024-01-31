a91cb2c8ee97f44aa131af7020e5ee3955969bdc
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-01-31 18:09:51 +0200

    Fruit Collision

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
+            // move sword with mouse
             var pos = Input.mousePosition;
             var worldPos = Camera.main.ScreenToWorldPoint(pos);
             worldPos.z = 0;
 
             transform.position = worldPos;
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print(":)");
+        Destroy(other.gameObject);
     }
 }
