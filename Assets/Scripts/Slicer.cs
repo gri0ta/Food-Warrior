0bcba6e758a54f921cbca9331ba81b9d433a4c05
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-01-31 19:27:00 +0200

    Slicer

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+        //transform.position = worldPos;
+
+        rb.MovePosition(worldPos);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(":)");
+        Destroy(collision.gameObject); //istrinu gameObject su kuriuo susilieciu
+    }
+}
