9d93c7ff7452adbd1b52c14e64109636ebcfd005
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-02-04 12:24:04 +0200

    Simple spawner

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -6,25 +6,16 @@ public class Food : MonoBehaviour
 {
     Rigidbody2D rb;
    // public Rigidbody2D fruit;
-    public GameObject fruits;
-    public float minSpawnTime = 0.5f;
-    public float maxSpawnTime = 1.5f;
+    
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, 11);
-        //InvokeRepeating("LaunchFruit", 2.0f, 0.3f);
-        InvokeRepeating("SpawnObject", 0.0f, Random.Range(minSpawnTime, maxSpawnTime));
+        
     }
-    void SpawnObject()
-    {
-        // Instantiate the prefab at the spawner's position
-        Instantiate(fruits, transform.position, Quaternion.identity);
-
-        // Reschedule the next spawn with random timing
-        Invoke("SpawnObject", Random.Range(minSpawnTime, maxSpawnTime));
-    }
+    
     void Update()
     {
         if (transform.position.y < -7.3)
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject fruitPrefab;
+    public float spawnSpeed = 1f;
+
     void Start()
     {
         
+        InvokeRepeating("Spawn", spawnSpeed, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        
+        var obj = Instantiate(fruitPrefab);
+        var x = Random.Range(-5f,5f);
+        obj.transform.position = new Vector3(x,-5,0);
     }
 }
